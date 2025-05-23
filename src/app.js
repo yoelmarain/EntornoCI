f2f5d12469e9cbd933a88c220f0da3b8835e17ea
yoelmarain <jmarain62@gmail.com>
2025-05-23 10:28:03 -0300

    add first test

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
+const concatenateNumbers = require('./concatenateNumbers');
+
 document.getElementById('concatButton').addEventListener('click', () => {
     const number1 = document.getElementById('number1').value;
     const number2 = document.getElementById('number2').value;
 
-    // Concatenar los números como cadenas
-    const result = number1 + number2;
+    const result = concatenateNumbers(number1, number2);
 
-    // Mostrar el resultado
     document.getElementById('result').textContent = `Resultado: ${result}`;
 });
---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+const concatenateNumbers = require('../src/concatenateNumbers');
+
+test('concatenateNumbers debería concatenar strings como números', () => {
+    expect(concatenateNumbers("3", "4")).toBe("34");
+});
+
+
