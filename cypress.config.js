bc9f24e7035d610e2ea66687dd50234856f74706
yoelmarain <jmarain62@gmail.com>
2025-05-25 21:09:00 -0300

    Refactor Cypress configuration: migrate from cypress.config.js to cypress.config.mjs

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-// Archivo de configuracion que dice a Cypress dónde están los tests
-
-module.exports = defineConfig({
-  e2e: {
-    specPattern: 'test/**/*.cy.js'
-  },
-});
---
+++ cypress.config.mjs
@@ -0,0 +1,7 @@
+// Archivo de configuración que dice a Cypress dónde están los tests
+
+export default {
+  e2e: {
+    specPattern: 'test/**/*.cy.js'
+  },
+};
