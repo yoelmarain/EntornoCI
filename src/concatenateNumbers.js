1a6390dc562e8591d6c61773fb43584428e19866
yoelmarain <jmarain62@gmail.com>
2025-05-27 09:33:57 -0300

    Add input validation to concatenateNumbers function

--- src/concatenateNumbers.js
+++ src/concatenateNumbers.js
@@ -1,5 +1,10 @@
 function concatenateNumbers(num1, num2) {
-    return num1 + num2;
+    if (!isNaN(num1) && !isNaN(num2)) {
+        return num1 + num2;
+    } else {
+        return 'Se deben ingresar dos números válidos';
+    }
+    
 }
 
 // CommonJS export (para Node/Jest)
