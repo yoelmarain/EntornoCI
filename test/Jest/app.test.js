6dd73da20a0899f97b353dafd2727d9068ed5b02
yoelmarain <134217892+yoelmarain@users.noreply.github.com>
2025-06-02 18:40:24 -0300

    rompiendo en jest

--- test/Jest/app.test.js
+++ test/Jest/app.test.js
@@ -1,7 +1,7 @@
 const concatenateNumbers = require("../../src/concatenateNumbers");
 
 test('concatenateNumbers debería concatenar strings como números', () => {
-    expect(concatenateNumbers("3", "4")).toBe("34");
+    expect(concatenateNumbers("3", "4")).toBe("344");
 });
 
 test('concatenateNumbers debería devolver mensaje de error si algún input está vacío', () => {
